5fd8084f16e7d834dfb543a8b013bae8ef25cc00
Patricia Urdaneta <dario.soto@gmail.com>
2021-04-04 05:27:39 -0400

    refactor
    the return of read only properties and updates on str and docstrings to match pep8

--- sections.py
+++ sections.py
@@ -1,24 +1,37 @@
 class Area:
-    """The representation of a room in a maze, its property is_portal indicates
-    that this area is an entrance or exist point of the maze.
+    """The representation of a room in a maze.
+    There are two types of areas portals and rooms, portals represent an
+    entrance or exist point of the maze.
     """
     __id = 0
 
     def __init__(self, is_portal: bool = False):
-        self.id = Area.__id + 1
+        self._id = Area.__id + 1
         Area.__id += 1
         self.is_portal = is_portal
-        self.links = []
+        self._links = []
 
     def __str__(self):
+        links = len(self.links)
         if self.is_portal:
-            return f"area marked as '{self.id}' has a portal"
-        if len(self.links) < 2:
-            return f"area marked as '{self.id}' is a dead end"
-        return f"area marked as '{self.id}'"
+            return f"room '{self.id}' has a portal"
+        if links < 2:
+            return f"room '{self.id}' is a dead end"
+        string = f"room '{self.id}' leads to another room"
+        if links > 2:
+            string += f" links to {links - 2} external areas"
+        return string
 
     def __repr__(self):
-        return f"{'portal' if self.is_portal else 'area'} '{self.id}'"
+        return f"{'portal' if self.is_portal else 'room'} '{self.id}'"
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def links(self) -> list:
+        return self._links
 
 
 class Hall:
@@ -27,35 +40,53 @@ class Hall:
     """
     __id = 0
 
-    def __init__(self, areas: list = None):
-        self.id = Hall.__id + 1
+    def __init__(self, areas: list[Area] = None):
+        self._id = Hall.__id + 1
         Hall.__id += 1
-        self.areas = areas if areas else []
+        self._areas = areas if areas else []
 
     def __str__(self):
         string = f"{'path' if self.is_path else 'branch'} '{self.id}'"
         if self.passages:
-            string += " joint on"
-        for joint in self.passages:
-            string += f" ({joint[0]} with {joint[1]})"
+            string += " other halls can be reached from"
+        passages = len(self.passages)
+        for passage in self.passages:
+            string += f" {passage[0]} to {passage[1]}"
+            if
         return string
 
     def __repr__(self):
-        return self.__str__()
+        string = f"{'path' if self.is_path else 'branch'} '{self.id}'"
+        if self.passages:
+            string += " passages on"
+        for passage in self.passages:
+            string += f" ({passage[0]} to {passage[1]})"
+        return string
 
     @property
-    def portals(self):
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def areas(self) -> list:
+        return self._areas
+
+    @property
+    def portals(self) -> list:
         return [area for area in self.areas if area.is_portal]
 
     @property
-    def is_path(self):
-        """A hall is a path if it has any portals, otherwise its a hall."""
+    def is_path(self) -> bool:
+        """The hall is a path if any of its areas are portals, otherwise
+        its a branch.
+        """
         return True if self.portals else False
 
     @property
-    def passages(self):
-        """List of area tuples where other halls branch from. The first element
-        is part of the current hall while the other belongs to the connected hall.
+    def passages(self) -> list[tuple[Area, Area]]:
+        """Entrances to other halls.
+        The first element belongs to this hall, the second one is part
+        of the branching hall.
         """
         all_links = []
         for area in self.areas:
@@ -74,9 +105,9 @@ class Maze:
     __id = 0
 
     def __init__(self, branches: list = None):
-        self.id = Maze.__id + 1
+        self._id = Maze.__id + 1
         Maze.__id += 1
-        self.halls = branches if branches else []
+        self._halls = branches if branches else []
 
     def __str__(self):
         portals = []
@@ -92,6 +123,14 @@ class Maze:
 
     def __repr__(self):
         return self.__str__()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def halls(self):
+        return self._halls
 
     @property
     def branches(self):
--- wizard.py
+++ wizard.py
@@ -2,14 +2,14 @@ import sections
 import random
 
 
-def _summon_area(
-        with_portal: bool = False,
-        linked_to: sections.Area = None) -> sections.Area:
-    """Magic spell to get a new maze area,
+def _summon_area(with_portal: bool = False,
+                 linked_to: sections.Area = None) -> sections.Area:
+    """Magic spell to get a new maze area. Defaults to a single room.
 
     Keyword arguments:
-    with_portal -- indicate if the area holds a portal (default False)
-    linked_to -- connects the new area with the given one (default None)
+
+    - with_portal -- the area will hold a portal (default False)
+    - linked_to -- connects the area with the given one (default None)
     """
     new_area = sections.Area(with_portal)
     if linked_to:
@@ -18,19 +18,20 @@ def _summon_area(
     return new_area
 
 
-def _summon_hall(
-        length: int = 1,
-        branching_from: sections.Area = None,
-        has_start: bool = False,
-        has_end: bool = False) -> sections.Hall:
+def _summon_hall(length: int = 1,
+                 branching_from: sections.Area = None,
+                 has_start: bool = False,
+                 has_end: bool = False) -> sections.Hall:
     """Magic spell to a get new maze hall.
-
     All areas in th hall are sequentially connected.
+    Defaults to a single portal area.
+
     Keyword arguments:
-    length --  number of areas in the hall (default 1)
-    branching_from --  an area from another hall (default None)
-    has_start --  indicates that the first area as a portal (default False)
-    has_end --  indicates that the last area is a portal (default False)
+
+    - length --  number of areas in the hall (default 1)
+    - branching_from --  an area from another hall (default None)
+    - has_start --  first area is a portal (default False)
+    - has_end --  last area is a portal (default False)
     """
     if length == 1 and not branching_from:
         return sections.Hall([sections.Area(True)])
@@ -53,26 +54,28 @@ def _find_connectable_areas(halls: list, link_limit: int) -> list:
     """Returns a list of areas with less links than link_limit."""
     areas = []
     for hall in halls:
-        available = [area for area in hall.areas if len(area.links) < link_limit]
+        available = [area for area in hall.areas
+                     if len(area.links) < link_limit]
         areas.extend([area for area in available])
     return areas
 
 
-def cast_maze(
-        portals: int = 1,
-        halls: int = 1,
-        branching_limit: int = 4,
-        hall_length_range: tuple = (1, 1)):
-    """Magic spell to creates a maze,
+def cast_maze(portals: int = 1,
+              halls: int = 1,
+              branching_limit: int = 4,
+              hall_length_range: tuple = (1, 1)) -> sections.Maze:
+    """Magic spell to creates a maze. Defaults to a single portal room.
 
-     within the limits of the requested parameters, defaults to a single room.
     Keyword arguments:
-    portals -- the number of portals in the maze, where a path can have a max of 2 portals
-    halls -- the number halls in the maze
-    branching_limit -- max number of branches that can spread from an area, where:
-      the minimum value is 1 for mazes of one room using same portal for entrance and exit
-      the minimum value is 2 for mazes of one hall, with one area as entrance and the other as exit
-    hall_length_range -- min-max number of areas a hall can have
+
+    - portals -- the number of portals in the maze (default 1)
+    - halls -- the number halls in the maze (default 1)
+    - branching_limit -- max number of links in any area (default 4)
+
+      - if the value is 1 the maze will be a portal room.
+      - if value is 2 the maze will be a two room path.
+
+    - hall_length_range -- min-max number of areas a hall can have
     """
     halls_to_build = halls
     length_min = hall_length_range[0]
@@ -90,9 +93,10 @@ def cast_maze(
     portals_to_build = portals - len(main_path.portals)
 
     for i in range(halls_to_build):
-        connections = _find_connectable_areas(new_maze.halls, branching_limit)
+        connections = _find_connectable_areas(
+            new_maze.halls, branching_limit)
         hall_length = random.randint(length_min, length_max)
-        branch_joint = random.choice(connections) if connections else None
+        branch_from = random.choice(connections) if connections else None
 
         add_entrance = False
         add_exit = False
@@ -105,7 +109,8 @@ def cast_maze(
             if add_entrance or add_exit:
                 portals_to_build -= 1
 
-        new_branch = _summon_hall(hall_length, branch_joint, add_entrance, add_exit)
+        new_branch = _summon_hall(
+            hall_length, branch_from, add_entrance, add_exit)
         new_maze.halls.append(new_branch)
 
     return new_maze
