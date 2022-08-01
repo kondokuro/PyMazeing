a27c8ed0f5d33fb9eab0d188b0682db86303dbaf
dario <dario.soto@gmail.com>
2022-07-31 21:00:12 -0400

    new skill
    a rogue skill to anotate the hall coordinates

--- rogue.py
+++ rogue.py
@@ -1,14 +1,27 @@
 """
-Contains the definitions of the rouge skills used for navigating a maze.
+Contains the definitions of the rogue skills used for navigating a maze.
 """
 import errors
 import sections
 
-def find_paths(maze: sections.Maze) -> list(sections.Hall):
+
+def find_paths(maze: sections.Maze) -> list[sections.Hall]:
+    """"""
     pass
 
-def detect_branches(maze: sections.Maze) ->list(sections.Hall):
+
+def detect_branches(maze: sections.Maze) -> list[sections.Hall]:
+    """With this skill the rogue can map out all the dead ends in a maze."""
     pass
 
-def track_portals(maze: sections.Maze) -> list(sections.Area):
+
+def track_portals(maze: sections.Maze) -> list[sections.Area]:
+    """This skill allows the rogue to identify the location of all portals 
+    in a maze.
+    """
+    pass
+
+
+def gather_coordinaes(hall: sections.Hall) -> list[tuple[int]]:
+    """This skill notes the coordinates that occupies the hall."""
     pass
