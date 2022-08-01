3948f1eb6ee638a04c4df2c4b9275b4e53c4270b
dario <dario.soto@gmail.com>
2022-08-01 17:07:30 -0400

    removing old code
    replacing spell code with placeholder comments

--- src/characters/wizard.py
+++ src/characters/wizard.py
@@ -37,31 +37,22 @@ def summon_hall(length: int = 1,
 
     # length considerations starting from inxex zero
     for i in range(length - 1):
-        new_area = sections.Area(new_hall.areas[i].coordinates)
-        new_hall.areas.append(new_area)
+        new_area = sections.Area((0, 0))
+        # new_hall.areas.append(new_area)
 
     # create resulting hall
     # new_hall = sections.Hall()
     #end = _summon_area(has_end, new_hall.areas[-1])
     # new_hall.areas.append()
-
-    return new_hall
-
-
-def _find_connectable_areas(halls: list, link_limit: int) -> list:
-    """Returns a list of areas with less links than link_limit."""
-    areas = []
-    for hall in halls:
-        available = [area for area in hall.areas
-                     if len(area.links) < link_limit]
-        areas.extend([area for area in available])
-    return areas
+    areas = [sections.Area(), ]
+    return sections.Hall(areas)
 
 
 def conjure_maze(portals: int = 1,
                  halls: int = 1,
-                 branching_limit: int = 4,
-                 hall_length_range: tuple = (1, 1)) -> sections.Maze:
+                 branching_limit: int = 0,
+                 min_hall_length: int = 1,
+                 max_hall_length: int = 1) -> sections.Maze:
     """Magic spell to creates a maze. Defaults to a single portal room.
 
     Keyword arguments:
@@ -76,40 +67,33 @@ def conjure_maze(portals: int = 1,
     - hall_length_range -- min-max number of areas a hall can have
     """
     halls_to_build = halls
-    length_min = hall_length_range[0]
-    length_max = hall_length_range[1]
-    new_maze = sections.Maze()
+    length_min = min_hall_length
+    length_max = max_hall_length
+    new_maze = sections.Maze([sections.Hall()])
 
     hall_length = random.randint(length_min, length_max)
     add_exit = True if portals > 1 and length_max > 1 else False
     main_path = sections.Hall(hall_length, None, True, add_exit)
-    new_maze.halls.append(main_path)
+    # new_maze.halls.append(main_path)
     if halls == 1:
         return new_maze
 
     halls_to_build -= 1
-    portals_to_build = portals - len(main_path.portals)
+    #portals_to_build = portals - len(main_path.portals)
 
-    for i in range(halls_to_build):
-        connections = _find_connectable_areas(
-            new_maze.halls, branching_limit)
-        hall_length = random.randint(length_min, length_max)
-        branch_from = random.choice(connections) if connections else None
+    add_entrance = False
+    add_exit = False
+    if portals_to_build > 0:
+        entrance_or_exit = random.choice((True, False))
+        if entrance_or_exit:
+            add_entrance = random.choice((True, False))
+        else:
+            add_exit = random.choice((True, False))
+        if add_entrance or add_exit:
+            portals_to_build -= 1
 
-        add_entrance = False
-        add_exit = False
-        if portals_to_build > 0:
-            entrance_or_exit = random.choice((True, False))
-            if entrance_or_exit:
-                add_entrance = random.choice((True, False))
-            else:
-                add_exit = random.choice((True, False))
-            if add_entrance or add_exit:
-                portals_to_build -= 1
-
-        new_branch = sections.Hall(
-            hall_length, branch_from, add_entrance, add_exit)
-        new_maze.halls.append(new_branch)
+        new_branch = sections.Hall()
+        # new_maze.halls.append(new_branch)
 
     return new_maze
 
