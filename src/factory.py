ea2bc6cd75151b7219162554cd4799c8b5d37e62
dario <dario.soto@gmail.com>
2023-03-04 22:29:57 -0500

    adding factory tests
    defined an enum for area shapes and added a test for the wizzard method for creating areas

--- src/factory.py
+++ src/factory.py
@@ -5,40 +5,73 @@ A Maze is composed of multiple areas.
 A Maze must have at least one Portal.
 
 Areas can have zero or many Portals
-Areas coordinates are unique, as in no two zones have the same coordinates
-Non connected Areas are separated by a space of size 1
+Areas coordinates are unique, no two have the same coordinates
+Areas are separated by walls
+Walls are objects inside of the area space
 
-Portals are located in Zones
+Portals are located in Areas
 Portals define entrance or exit from a Maze
-Portals can connect different Mazes
+Portals connect two Mazes
 """
+from enum import Enum, auto
 from src import components, systems
 
 
-class Wizzard:
-    """Generates mazes with just a few parametes."""
+class AreaShape(Enum):
+    """The different generic area shapes found in a maze, basd on their available open access."""
 
-    def cast_maze(self, name: str, origin: systems.Coordinate, branches: int, *args, **kwargs) -> components.Maze:
+    CLOSED = auto()
+    DEAD_END_W = auto()
+    DEAD_END_S = auto()
+    DEAD_END_E = auto()
+    DEAD_END_N = auto()
+    WAY_WE = auto()
+    WAY_NS = auto()
+    CORNER_WS = auto()
+    CORNER_SE = auto()
+    CORNER_EN = auto()
+    CORNER_NW = auto()
+    JUNCTION_WSE = auto()
+    JUNCTION_WNE = auto()
+    CROSSROAD = auto()
+
+
+class Wizzard:
+    """Summon mazes with just a few parametes."""
+
+    def _summon_maze(self, name: str, origin: systems.Coordinate) -> None:
+        """Instantiates an initial maze for the wizzard"""
+        self._maze = components.Maze(name, origin)
+
+    def cast_maze(
+        self, name: str, origin: systems.Coordinate, branches: int, *args, **kwargs
+    ) -> components.Maze:
         """
         Generates an initial maze to build up from.
-        
+
         :param name: the label of the maze.
         :param origin: the initial location of the maze.
         :param branches: the total number of branches the maze will contain.
         """
-        return components.Maze(name, origin)
+        self._summon_maze(name, origin)
+        return self._maze
 
     def cast_area(
-        self, position: systems.Coordinate, size: systems.Size) -> components.Area:
+        self,
+        maze: components.Maze,
+        shape: AreaShape,
+        location: systems.Coordinate,
+        portal: bool = False,
+        size: systems.Size = systems.Size(),
+    ) -> components.Area:
         """
         Creates areas surrounded by walls based on the parameters provided.
 
-        :param position: Coordinates from the center of the area.
-        :param size: Space the area will occupy
+        :param shape: An AreaShape enum to give the area its open spaces and walls
+        :param location: The top left Coordinate of the area.
+        :param size: Space the area will occupy TBD
         """
-
-        # must check if the maze space is free.. so maybe this should be part of the mase class
-        return components.Area("WIP", position, size)
+        return components.Area("WIP", location, size, maze.id)
 
     def branch(self, area: components.Area) -> systems.TypedList[components.Area]:
         """The idea here is to take an area as the starting point and build a group of areas."""
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -9,3 +9,33 @@ class TestForWizzard:
         Labyrinth = self.mage.cast_maze("Test Labyrinth", origin, 5)
         assert isinstance(Labyrinth, components.Maze)
         assert origin == Labyrinth.position
+
+    @pytest.mark.parametrize(
+            "shape, walls",
+            [
+                (factory.AreaShape.CLOSED, 8),
+                (factory.AreaShape.DEAD_END_E, 7),
+                (factory.AreaShape.DEAD_END_N, 7),
+                (factory.AreaShape.DEAD_END_W, 7),
+                (factory.AreaShape.DEAD_END_S, 7),
+                (factory.AreaShape.CORNER_EN, 6),
+                (factory.AreaShape.CORNER_NW, 6),
+                (factory.AreaShape.CORNER_SE, 6),
+                (factory.AreaShape.CORNER_WS, 6),
+                (factory.AreaShape.WAY_NS, 6),
+                (factory.AreaShape.WAY_WE, 6),
+                (factory.AreaShape.JUNCTION_WNE, 5),
+                (factory.AreaShape.JUNCTION_WNE, 5),
+                (factory.AreaShape.CROSSROAD, 4),
+            ],
+        )
+    def test_CastArea_AreaTypes_AreaHasEnoughWalls(self, shape, walls):
+        labyrinth = self.mage.cast_maze("corn", systems.Coordinate(), 5)
+        area = self.mage.cast_area(labyrinth, shape, systems.Coordinate())
+        assert len(area.content) == walls
+
+    def test_CastArea_InitialLocation_AreaSizeIsThree(self):
+        initial = systems.Coordinate(5, -4)
+        area = self.mage.cast_area(factory.AreaShape.CLOSED, initial)
+        assert area.size.length == 3
+        assert area.size.height == 3
